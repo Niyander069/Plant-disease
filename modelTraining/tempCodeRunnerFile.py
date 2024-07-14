ed8a9371e8c9ed13db03a62b6e73a16e3724c516
unknown <saifulislam892002@gmail.com>
2024-07-14 17:55:31 +0530

    first commit

---
+++ modelTraining/tempCodeRunnerFile.py
@@ -0,0 +1,19 @@
+import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import os
+
+# import keras
+# from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+# from keras.applications.vgg19 import VGG19, preprocess_input, decode_predictions
+
+# len(os.listdir(r"E:\mini project\Plant-Disease-Detection-And-Prevention\Dataset\train"))
+
+
+# train_datagen = ImageDataGenerator(zoom_range=0.5, shear_range=0.3, horizontal_flip=True, preprocessing_function=preprocess_input)
+# val_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+
+
+# train = train_datagen.flow_from_directory(directory=r"E:\mini project\Plant-Disease-Detection-And-Prevention\Dataset\train",target_size=(256,256), batch_size=32)
+
+# val = val_datagen.flow_from_directory(directory=r"E:\mini project\Plant-Disease-Detection-And-Prevention\Dataset\val", target_size=(256,256), batch_size=32)
